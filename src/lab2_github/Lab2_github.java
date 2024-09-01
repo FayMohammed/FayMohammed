bf08d62feed0e525d35a154f739770650cadbca5
Admin <Admin@WINDOWS-ISFVGL7>
2024-09-01 16:24:05 +0300

    trying out github Repository

---
+++ src/lab2_github/Lab2_github.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_github;
+
+/**
+ *
+ * @author fmararubaya@stu.kau.edu.sa
+ */
+public class Lab2_github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hello github");
+    }
+    
+}
